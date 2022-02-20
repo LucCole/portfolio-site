fe794bb95d4d5833e13d7427547d08355ab52e76
LucCole <luccoleb@yahoo.com>
2022-02-20 00:21:57 -0500

    setup react

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = (props) => {
+  return <>
+  heyd
+  </>
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app'),
+);
